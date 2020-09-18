4dd7a9d553bfb205092e4f8334ff77188e65fcca
huahua <799431668@qq.com>
2020-09-19 00:21:33 +0800

    分类左侧类目接口请求以及数据模型准备

--- flutter_shop/lib/config/const_url.dart
+++ flutter_shop/lib/config/const_url.dart
@@ -1,8 +1,25 @@
 class kUrl {
 
+/*
+ *  首页数据
+ */ 
+// 
 static const String home_getHomePageContent_url = "getHomePageContent";
 
+// 热门商品
 static const String home_getHotGoods = "getHotGoods";
+
+
+/*
+ *  分类数据
+ */ 
+
+// 一级类目数据
+static  const String category_getCategory = "getCategory";
+
+// 二级类目数据
+static  const String category_getSubCategory = "getCategory";
+
 
 
 }
--- flutter_shop/lib/pages/category_page.dart
+++ flutter_shop/lib/pages/category_page.dart
@@ -1,5 +1,11 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_shop/service/http_service.dart';
+import '../config/index.dart';
+
+import '../model/category_model.dart';
 
 
 class CategoryPage extends StatefulWidget {
@@ -12,10 +18,130 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
-   return Container(
-       child: Center(
-         child: Text("分类"),
+   return Scaffold(
+     appBar: AppBar(
+
+       title: Text(kString.categoryTitle),
+     ),
+     body: Container(
+
+       child: Row(
+
+
+         children: <Widget>[
+
+           CategoryLeftView(),
+           Column(
+             children: <Widget>[
+               CategoryRightView(),
+               CategoryGoodsListView(),
+
+             ],
+
+           )
+
+         ],
+         
        ),
+        
+     )
+   );
+  }
+}
+
+
+// 左侧视图
+class CategoryLeftView extends StatefulWidget {
+  @override
+  _CategoryLeftViewState createState() => _CategoryLeftViewState();
+}
+
+class _CategoryLeftViewState extends State<CategoryLeftView> {
+
+   // 分类左侧一级类目数组
+   List categoryLeftList = [];
+   var  selectedIndex = 0; //默认选中第一个
+
+
+
+  // 拉取分类左测数据
+  @override
+  void initState() {
+    super.initState();
+    // 一初始化就掉分类左侧数据
+    _getCategoryData();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+
+
+  // 获取分类数据
+_getCategoryData() async{
+
+  await request(kUrl.category_getCategory,formData: null).then((value){
+
+    var data = json.decode(value.toString());
+//    print('分类数据$data');
+
+     CategoryModel model = CategoryModel.fromJson(data);
+
+     setState(() {
+
+       categoryLeftList = model.data;
+
+     });
+    
+  });
+
+
+}
+
+}
+
+
+// 右侧视图
+class CategoryRightView extends StatefulWidget {
+  @override
+
+  _CategoryRightViewState createState() => _CategoryRightViewState();
+}
+
+
+class _CategoryRightViewState extends State<CategoryRightView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
     );
   }
 }
+
+
+
+// 商品列表
+class CategoryGoodsListView extends StatefulWidget {
+  @override
+  _CategoryGoodsListViewState createState() => _CategoryGoodsListViewState();
+}
+
+class _CategoryGoodsListViewState extends State<CategoryGoodsListView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
+
+
+
+  
+
+
