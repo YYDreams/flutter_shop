458f02c5277d6f63cc5c4fe3a7c66d98af6e11ad
huahua <799431668@qq.com>
2020-11-10 20:26:57 +0800

    解决网络请求bug,明天将网络请求封装一下

--- flutter_shop/lib/pages/goods_detail_page.dart
+++ flutter_shop/lib/pages/goods_detail_page.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_shop/config/const_url.dart';
 import 'package:flutter_shop/service/http_service.dart';
 import 'dart:convert';
-
+import 'package:dio/dio.dart';
+import 'dart:async';
 
 class GoodsDetailPage extends StatelessWidget {
 
@@ -32,29 +33,29 @@ class GoodsDetailPage extends StatelessWidget {
             future: _getGoodsInfo(context),
             builder: (context,snapshot){
 
-//              if(snapshot.hasData){
-//
-//                return Stack(
-//
-//                  children: [
-//
-//                    ListView(
-//
-//                      children: [
-//
-//                      ],
-//                    ),
-//                    //底层元素
-//                    Positioned(
-//                      bottom: 0,
-//                        left: 0,
-//                        child: Text("底部组件"))
-//                  ],
-//                );
-//              }else{
-//                return Text("加载中....");
-//
-//              }
+              if(snapshot.hasData){
+
+                return Stack(
+
+                  children: [
+
+                    ListView(
+                      
+                      children: [
+
+                      ],
+                    ),
+                    //底层元素
+                    Positioned(
+                      bottom: 0,
+                        left: 0,
+                        child: Text("底部组件"))
+                  ],
+                );
+              }else{
+                return Text("加载中....");
+
+              }
 
             },
 
@@ -71,14 +72,19 @@ class GoodsDetailPage extends StatelessWidget {
     var formData = {'goodId':'001'};
 
 
-    await request(kUrl.getGoodDetail ,formData:formData).then((value){
+    Dio dio = Dio();
+
+    dio.options.contentType = Headers.formUrlEncodedContentType;
+
+
+    await dio.post("http://127.0.0.1:3000/getGoodDetail").then((value){
+
 
       var responseData  = json.decode(value.toString());
 
       print("responseData:" + responseData.toString());
 
     });
-
 
 
   }
